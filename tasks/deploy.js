0fed9b339933b3c217a9a4bc062eb1770a843be5
Vikas Sah <vikassah@gmail.com>
2022-03-13 00:37:02 -0500

    added deploy.js

---
+++ tasks/deploy.js
@@ -0,0 +1,50 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  //const LoyaltyERCToken = await hre.ethers.getContractFactory("LoyaltyERC20");
+  //const loyalty = await LoyaltyERCToken.deploy("Loyalty Coin", "LOYL", 1000000);
+
+  const LoyaltyCoinFactory = await hre.ethers.getContractFactory("LoyaltyCoinFactory");
+  const factory = await LoyaltyCoinFactory.deploy();
+  await factory.deployed();
+
+  console.log("LoyaltyCoinFactory deployed to:", factory.address);
+
+  await factory.createLoyaltyERC20Coin("MOES Coin", "MOES", 1000000);
+  await factory.createLoyaltyERC20Coin("Starbucks Coin", "SBUCKS", 1000000);
+  await factory.createLoyaltyERC20Coin("MOES Coin", "LOYL", 1000000);
+  await factory.createLoyaltyERC20Coin("Starbucks Coin", "MCD", 1000000);
+
+  console.log("Total coins:", (await factory.totalCoins()).toNumber());
+
+  let loyaltyCoin = await factory.getCoinAddressBySymbol("LOYL");
+
+  console.log(loyaltyCoin);
+
+  loyaltyCoin = await factory.getCoinAddressBySymbol("SBUCKS");
+
+  console.log(loyaltyCoin);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
