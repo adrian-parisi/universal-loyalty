0773a38f5cda7310ae9aad5d75e5c16da62ecdb3
Vikas Sah <vikassah@gmail.com>
2022-03-13 21:55:36 -0400

    updated with event emission for coin creation

--- contracts/LoyaltyCoinFactory.sol
+++ contracts/LoyaltyCoinFactory.sol
@@ -11,12 +11,13 @@ contract LoyaltyCoinFactory {
 
     mapping(string => address) public loyaltyCoins;
     uint256 public totalCoins;
+    event CoinCreated(address indexed _creator, string indexed _tokenSymbol, uint _totalSupply);
 
     // change permissions later to only allow brand addresses authorized to create a coin
     function createLoyaltyERC20Coin(string memory  _tokenName, string memory _tokenSymbol, uint256 _totalSupply) public  {
         loyaltyCoins[_tokenSymbol] = address(new LoyaltyERC20(_tokenName, _tokenSymbol, _totalSupply, msg.sender));
-
         totalCoins++;
+        emit CoinCreated(msg.sender, _tokenSymbol, _totalSupply);
      }
 
     //
