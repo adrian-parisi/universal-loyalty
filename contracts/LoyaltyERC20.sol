c27013c8181120eee502b4205a2122835ab9d004
Vikas Sah <vikassah@gmail.com>
2022-03-16 22:01:26 -0400

    bug fixes and support for redeem

--- contracts/LoyaltyERC20.sol
+++ contracts/LoyaltyERC20.sol
@@ -10,23 +10,39 @@ import "hardhat/console.sol";
 
 contract LoyaltyERC20 is ERC20 {
     address public owner;
+    mapping(address => bool) public admins;
     //add admin users later
 
-    constructor(string memory  _tokenName, string memory _tokenSymbol, uint256 _totalSupply, address _owner) ERC20(_tokenName, _tokenSymbol) {
-        _mint(msg.sender, _totalSupply * (10 ** 18));
+    constructor(string memory  _tokenName, string memory _tokenSymbol, uint256 _totalSupply, address _owner) 
+                ERC20(_tokenName, _tokenSymbol) {
         owner = _owner;
+        admins[_owner] = true;
+        _mint(_owner, _totalSupply);
     }
 
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount * 10 ** 18);
+    function mint(address to, uint256 amount) external onlyOwnerAndAdmin {
+        _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) external {
-        _burn(from, amount * 10 ** 18);
+    function burn(address from, uint256 amount) external onlyOwnerAndAdmin {
+        _burn(from, amount);
     }
 
     modifier onlyOwner {
         require(msg.sender == owner, "not the owner");
         _;
+    }
+
+    modifier onlyOwnerAndAdmin {
+        require(msg.sender == owner || admins[msg.sender], "not admin or owner");
+        _;
+    }
+
+    function addAdmin(address _admin) external onlyOwner {
+        admins[_admin] = true;
+    }
+
+    function deactivateAdmin(address _admin) external onlyOwner {
+        admins[_admin] = false;
     }
 }
